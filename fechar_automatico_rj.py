2c6ddedc563c4d2da9cc1a80cd50c195cb68ea5a
Gabriel Rangel <gabrielrangel.001@gmail.com>
2023-01-09 13:32:34 -0300

    Entrega da versão 3.0 do workstation

--- fechar_automatico_rj.py
+++ fechar_automatico_rj.py
@@ -1,8 +1,8 @@
 import model
 Agenda = model.Agenda()
 
-Agenda.fechar_agendas('São Cristóvão', 'vaccines', 10)
-Agenda.fechar_agendas('São Cristóvão', 'laboratories', 10)
-Agenda.fechar_agendas('Barra', 'vaccines', 10)
-Agenda.fechar_agendas('Barra', 'laboratories', 10)
-Agenda.fechar_agendas('Cabo Frio', 'vaccines', 10)
+Agenda.fechar_agendas('São Cristóvão', 'vaccines', 3)
+Agenda.fechar_agendas('São Cristóvão', 'laboratories', 3)
+Agenda.fechar_agendas('Barra', 'vaccines', 3)
+Agenda.fechar_agendas('Barra', 'laboratories', 3)
+Agenda.fechar_agendas('Cabo Frio', 'vaccines', 3)
--- fechar_automatico_sp.py
+++ fechar_automatico_sp.py
@@ -1,10 +1,10 @@
 import model
 Agenda = model.Agenda()
 
-Agenda.fechar_agendas('Tatuapé', 'vaccines', 7)
-Agenda.fechar_agendas('Tatuapé', 'laboratories', 7)
-Agenda.fechar_agendas('Vila Olímpia', 'vaccines', 7)
-Agenda.fechar_agendas('Vila Olímpia', 'laboratories', 7)
-Agenda.fechar_agendas('São Bernardo do Campo', 'vaccines', 7)
-Agenda.fechar_agendas('São Bernardo do Campo', 'laboratories', 7)
-Agenda.fechar_agendas('Campinas', 'vaccines', 7)
+Agenda.fechar_agendas('Tatuapé', 'vaccines', 3)
+Agenda.fechar_agendas('Tatuapé', 'laboratories', 3)
+Agenda.fechar_agendas('Vila Olímpia', 'vaccines', 3)
+Agenda.fechar_agendas('Vila Olímpia', 'laboratories', 3)
+Agenda.fechar_agendas('São Bernardo do Campo', 'vaccines', 3)
+Agenda.fechar_agendas('São Bernardo do Campo', 'laboratories', 3)
+Agenda.fechar_agendas('Campinas', 'vaccines', 3)
